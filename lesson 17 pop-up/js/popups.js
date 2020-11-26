b31264ce2c978bcb9449a50f969a96b245411f2e
oleg7171 <71016826+oleg7171@users.noreply.github.com>
2020-11-26 10:17:07 +0300

    Создали 17 урок
    1

---
+++ lesson 17 pop-up/js/popups.js
@@ -0,0 +1,6 @@
+		/*		<div href="#header" class="popup__text">
+					<a clacc="header__btnYes popup-link" href="#header">Yes</a>
+				</div>
+				<div href="#header" class="popup__text">
+				    <a clacc="header__btnNo popup-link" href="#header">No</a>
+			    </div>*/
